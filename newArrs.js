dc59d5fd3a1d58087b0ec607f6445de4411e58c1
Adrian Ross <adriantoddross@gmail.com>
2018-01-16 15:11:13 -0500

    finished functions 2

--- newArrs.js
+++ newArrs.js
@@ -1,3 +1,4 @@
+'use strict';
 const repeat = (fn,n) => {
     for (let i=1;i<=n;i++) {
         fn();
@@ -13,4 +14,32 @@ const goodbye = () => {
     console.log('Goodbye World');
 }
 
-repeat(goodbye, 50);
+// repeat(goodbye, 50);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let currentElement = arr[i];
+    if (fn(currentElement) === true) {
+      newArray.push(currentElement);
+    } else {
+      // then do nothing!
+    }
+  }
+  return newArray;
+}
+console.log(filteredNames);
